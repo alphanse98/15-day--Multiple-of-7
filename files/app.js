f10d7be69dc1c58bde7b7fda31fefe28ec9a6cf2
alphanse <alphanse98@gmail.com>
2022-06-24 13:49:12 +0530

    add-file

---
+++ files/app.js
@@ -0,0 +1,9 @@
+let a = parseInt(prompt());
+if(a%7 ==0){
+  var p = document.getElementById('p').innerHTML = (a + "   is divisible by 7")
+}else{
+    x = document.getElementById('p').innerHTML = (a + "   is not divisible by 7")
+}
+
+
+
